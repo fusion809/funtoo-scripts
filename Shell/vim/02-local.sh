81155eb481cfef7c7ad98c367e1bf63d1f838b47
Brenton Horne <brentonhorne77@gmail.com>
2017-03-20 05:11:51 +1000

    Updating scripts

--- Shell/vim/02-local.sh
+++ Shell/vim/02-local.sh
@@ -1,5 +1,5 @@
 function ved {
-	VERSION=(ls -lh | cut -d ' ' -f 9 | grep -v "9999" | cut -d '-' -f 2 | sed 's/\.ebuild//g' | grep "[0-9]" | sort -u | tail -n 1)
+	VERSION=$(ls -lh | cut -d ' ' -f 9 | grep -v "9999" | cut -d '-' -f 2 | sed 's/\.ebuild//g' | grep "[0-9]" | sort -u | tail -n 1)
 	
 	if `echo $VERSION > /dev/null 2>&1`; then
 		VERSION=$(ls -lh | cut -d ' ' -f 9 | grep -v "9999" | cut -d '-' -f 3 | sed 's/\.ebuild//g' | grep "[0-9]" | sort -u | tail -n 1)
