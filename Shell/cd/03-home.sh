5b9fa67b7447c00e0f90027832b5a60495c7e622
Brenton Horne <brentonhorne77@gmail.com>
2017-03-23 19:35:40 +1000

    Replacing hard tabs with 5 spaces

--- Shell/cd/03-home.sh
+++ Shell/cd/03-home.sh
@@ -1,55 +1,59 @@
 function cda {
-	cd "$HOME/AUR/$1"
+    cd "$HOME/AUR/$1"
 }
 
 function cdp {
-	cd "$HOME/Programs/$1"
+    cd "$HOME/Programs/$1"
 }
 
 function cdpa {
-	cd "$HOME/Programs/AppImages"
+    cd "$HOME/Programs/AppImages"
 }
 
 function cdsh {
-	cd "$HOME/Shell/$1"
+    cd "$HOME/Shell/$1"
 }
 
 function cddo {
-	cd "$HOME/Downloads/$1"
+    cd "$HOME/Downloads/$1"
 }
 
 function cddc {
-	cd "$HOME/Documents/$1"
+    cd "$HOME/Documents/$1"
 }
 
 function cdcfe {
-	cddc "CodeLite/CPP-Math-Projects/$1"
+    cddc "CodeLite/CPP-Math-Projects/$1"
 }
 
 function cdle {
-	cdcfe "Lorenz-equations-mysolver/$1"
+    cdcfe "Lorenz-equations-mysolver/$1"
 }
 
 function cdsp {
-	cdcfe "Simple-Pendulum/$1"
+    cdcfe "Simple-Pendulum/$1"
 }
 
 function cdm {
-	cd $HOME/Music/$1
+    cd $HOME/Music/$1
 }
 
 function cdobs {
-	cd $HOME/OBS/$1
+    cd $HOME/OBS/$1
 }
 
 function cdobsh {
-	cdobs "home:fusion809/$1"
+    cdobs "home:fusion809/$1"
 }
 
 function cdvi {
-	cd $HOME/"VirtualBox VMs"/ISOs/$1
+    cd $HOME/"VirtualBox VMs"/ISOs/$1
 }
 
 function cdvs {
-	cd $HOME/Videos/SO/$1
+    cd $HOME/Videos/SO/$1
+}
+
+function cdvy {
+    cd $HOME/Videos/YouTube/$1
 }
