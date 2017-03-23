07f2a2d122662fde8204e6ce350901f6859d7987
Brenton Horne <brentonhorne77@gmail.com>
2017-03-23 18:14:42 +1000

    Replacing hard tabs with 5 spaces

--- Shell/pms/04-ebuild.sh
+++ Shell/pms/04-ebuild.sh
@@ -63,52 +63,56 @@ function ovimup {
               osc ci -m "Bumping to $pkgver"
               cd -
 
-			cd $HOME/OBS/home:fusion809/vim-debian
-			osc ci -m "Bumping to $pkgver"
-			cd -
+              cd $HOME/OBS/home:fusion809/vim-debian
+              osc ci -m "Bumping to $pkgver"
+              cd -
 
-			vimaup
-		fi
-	fi
+              vimaup
+         fi
+    fi
 
-	if [[ $baseversion != $vim_baseversion ]] || [[ $patchversion != $vim_patchversion ]]; then
-		osc ci -m "Bumping version to $pkgver"
-	fi
+    if [[ $baseversion != $vim_baseversion ]] || [[ $patchversion != $vim_patchversion ]]; then
+         osc ci -m "Bumping version to $pkgver"
+    fi
 }
 function vimup {
-	pkgver=$(wget -q https://github.com/vim/vim/releases -O - | grep "tar\.gz" | head -n 1 | cut -d '/' -f 5 | cut -d '"' -f 1 | sed 's/v//g' | sed 's/\.tar\.gz//g')
-	pushd $GHUBM/packaging/vim-overlay/app-editors/vim
-	lver_vim=$(ls | grep ebuild | sort -u | tail -n 2 | head -n 1 | cut -d '-' -f 2 | sed 's/\.ebuild//g')
-	if ! [[ $lver_vim == $pkgver ]]; then
-		mv vim-$lver_vim.ebuild vim-$pkgver.ebuild
-		sudo ebuild vim-$pkgver.ebuild manifest merge
-	fi
-	popd
-	pushd $GHUBM/packaging/vim-overlay/app-editors/vim-core
-	lver_vimc=$(ls | grep ebuild | sort -u | tail -n 2 | head -n 1 | cut -d '-' -f 3 | sed 's/\.ebuild//g')
-	if ! [[ $lver_vimc == $pkgver ]]; then
-		mv vim-core-$lver_vimc.ebuild vim-core-$pkgver.ebuild
-		sudo ebuild vim-core-$pkgver.ebuild manifest merge
-	fi
-	popd
-	pushd $GHUBM/packaging/vim-overlay/app-editors/gvim
-	lver_gvim=$(ls | grep ebuild | sort -u | tail -n 2 | head -n 1 | cut -d '-' -f 2 | sed 's/\.ebuild//g')
-	if ! [[ $lver_gvim == $pkgver ]]; then
-		mv gvim-$lver_gvim.ebuild gvim-$pkgver.ebuild
-		sudo ebuild gvim-$pkgver.ebuild manifest merge
-	fi
-	push "Bumping version to $pkgver"
-	popd
-	
-	printf '\e[1;34m%-0s\e[m' "Running ovimup vim."
-	printf "\n"
-	ovimup vim
 
-	printf '\e[1;34m%-0s\e[m' "Running ovimup vim-suse."
-	printf "\n"
-	ovimup "vim-suse"
+    pkgver=$(wget -q https://github.com/vim/vim/releases -O - | grep "tar\.gz" | head -n 1 | cut -d '/' -f 5 | cut -d '"' -f 1 | sed 's/v//g' | sed 's/\.tar\.gz//g')
+    pushd $GHUBM/packaging/vim-overlay/app-editors/vim
+    lver_vim=$(ls | grep ebuild | sort -u | tail -n 2 | head -n 1 | cut -d '-' -f 2 | sed 's/\.ebuild//g')
 
-	printf '\e[1;34m%-0s\e[m' "Running ovimup vim-redhat."
-	printf "\n"
-	ovimup "vim-redhat"
+    if ! [[ $lver_vim == $pkgver ]]; then
+        mv vim-$lver_vim.ebuild vim-$pkgver.ebuild
+        sudo ebuild vim-$pkgver.ebuild manifest merge
+    fi
+    popd
+
+    pushd $GHUBM/packaging/vim-overlay/app-editors/vim-core
+    lver_vimc=$(ls | grep ebuild | sort -u | tail -n 2 | head -n 1 | cut -d '-' -f 3 | sed 's/\.ebuild//g')
+    if ! [[ $lver_vimc == $pkgver ]]; then
+        mv vim-core-$lver_vimc.ebuild vim-core-$pkgver.ebuild
+        sudo ebuild vim-core-$pkgver.ebuild manifest merge
+    fi
+    popd
+
+    pushd $GHUBM/packaging/vim-overlay/app-editors/gvim
+    lver_gvim=$(ls | grep ebuild | sort -u | tail -n 2 | head -n 1 | cut -d '-' -f 2 | sed 's/\.ebuild//g')
+    if ! [[ $lver_gvim == $pkgver ]]; then
+        mv gvim-$lver_gvim.ebuild gvim-$pkgver.ebuild
+        sudo ebuild gvim-$pkgver.ebuild manifest merge
+    fi
+    push "Bumping version to $pkgver"
+    popd
+
+    printf '\e[1;34m%-0s\e[m' "Running ovimup vim."
+    printf "\n"
+    ovimup vim
+
+    printf '\e[1;34m%-0s\e[m' "Running ovimup vim-suse."
+    printf "\n"
+    ovimup "vim-suse"
+
+    printf '\e[1;34m%-0s\e[m' "Running ovimup vim-redhat."
+    printf "\n"
+    ovimup "vim-redhat"
 }
